8a635e580cd997d8b85f95ff9f201aa6ed33e75e
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-03-24 22:31:31 +0300

    solution added

--- python_flatten.py
+++ python_flatten.py
@@ -1,1 +1,13 @@
-print("hello")
+# h.halil olcay
+def flatten( list ):
+    for item in list:
+        if type( item ) == list:
+            flatten( item )       # recursive function
+        else:
+            result.append( item )
+
+
+if __name__ == '__main__':
+    result = []
+    flatten( [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5] )
+    print( result )
