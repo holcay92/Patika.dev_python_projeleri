3c76ee3271b5eb719f7a085d3f0a2fa358fd5054
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-03-25 00:48:50 +0300

    solution added

--- python_reverse.py
+++ python_reverse.py
@@ -2,15 +2,14 @@
 # input: [[1, 2], [3, 4], [5, 6, 7]]
 # output: [[[7, 6, 5], [4, 3], [2, 1]]
 
-
 def reverse_recursive(raw_list):
     raw_list.reverse()
 
     for sub in raw_list:
         if type(sub) == list:
             reverse_recursive(sub)
-            #
         else: result.append(sub)
+
 
 if __name__ == '__main__':
     raw_input = [[1, 2, [8, 0, 9]], [3, 4], [5, 6, 7]]
